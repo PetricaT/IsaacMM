8b360d7807a98c6606ae8582f3c33e1aaf0ee3d9
PetricaT <24418935+PetricaT@users.noreply.github.com>
2025-02-02 16:28:41 +0200

    Fixed syntax with black and flake8

--- gui.py
+++ gui.py
@@ -1,21 +1,36 @@
 from PySide6.QtGui import QIcon, QPalette
-from PySide6.QtWidgets import QApplication, QMainWindow, QListView, QAbstractItemView, QPushButton, QWidget, QGridLayout, QFileDialog
-from PySide6.QtCore import Qt, QStringListModel, QModelIndex, QMimeData, QByteArray, QDataStream, QIODevice
-from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtWidgets import (
+    QApplication,
+    QListView,
+    QAbstractItemView,
+    QPushButton,
+    QWidget,
+    QGridLayout,
+    QFileDialog,
+)
+from PySide6.QtCore import (
+    Qt,
+    QStringListModel,
+    QModelIndex,
+    QMimeData,
+    QByteArray,
+    QDataStream,
+    QIODevice,
+)
 import xml.etree.ElementTree as ET
 import getpass
 import toml
 import sys
 import os
-import re 
+import re
 
 
-sorted_pattern = re.compile(r'[0-9]{3}\s{1}.*')
+sorted_pattern = re.compile(r"[0-9]{3}\s{1}.*")
 
-mods_path = ''
-cfg_file = ''
+mods_path = ""
+cfg_file = ""
 
-version = 'v0.1.2'
+version = "v0.1.4"
 
 try:
     cfg_file = toml.load("./config.toml")
@@ -23,14 +38,16 @@ try:
         mods_path = cfg_file["paths"]["mods"]
     except:
         print("Mods path malformed, check if path is correct")
-        mods_path = ''
+        mods_path = ""
 except:
     print("Config file not found")
-    with open('./config.toml', 'w') as f:
+    with open("./config.toml", "w") as f:
         f.write("[paths]\n")
-        if sys.platform == 'darwin':
-            # official MacOS support was dropped, so we know the path is permanently this, we can just guess it.
-            f.write(f"mods='/Users/{getpass.getuser()}/Library/Application Support/Binding of Isaac Afterbirth+ Mods'")
+        if sys.platform == "darwin":
+            # Official MacOS support was dropped, so we know the path is permanently this, we can just guess it.
+            f.write(
+                f"mods='/Users/{getpass.getuser()}/Library/Application Support/Binding of Isaac Afterbirth+ Mods'"
+            )
         else:
             # Sadly, Linux support is weird, and Windows is a shot in the dark.
             f.write("mods=''")
@@ -41,7 +58,7 @@ class DragDropListModel(QStringListModel):
     def __init__(self, parent=None):
         super(DragDropListModel, self).__init__(parent)
 
-        self.myMimeTypes = 'application/json'
+        self.myMimeTypes = "application/json"
 
     def supportedDropActions(self):
         return Qt.MoveAction
@@ -118,7 +135,7 @@ class DragApp(QWidget):
     def __init__(self, parent=None):
         super(DragApp, self).__init__(parent)
 
-        self.setWindowTitle(f'Tboi Mod Manager [{version}]')
+        self.setWindowTitle(f"Tboi Mod Manager [{version}]")
         self.resize(480, 320)
 
         self.initUi()
@@ -156,20 +173,20 @@ class DragApp(QWidget):
         self.refreshOrder = QPushButton("Refresh")
         self.pickModsPath = QPushButton("Select Mods Folder")
 
-        self.applyOrder.setStyleSheet(f"background-color : {self.accent_color}; color : white;") 
+        self.applyOrder.setStyleSheet(
+            f"background-color : {self.accent_color}; color : white;"
+        )
 
-        if mods_path == '':
-            self.pickModsPath.setStyleSheet(f"background-color : red; color : white;")
+        if mods_path == "":
+            self.pickModsPath.setStyleSheet("background-color : red; color : white;")
         else:
-            self.pickModsPath.setStyleSheet(f"background-color: auto; color: auto")
-
+            self.pickModsPath.setStyleSheet("background-color: auto; color: auto")
 
     def setModsPath(self):
-        print('Presenting file dialog')
+        print("Presenting file dialog")
         cfg_file = toml.load("./config.toml")
         mods_path = QFileDialog.getExistingDirectory(self)
         cfg_file["paths"]["mods"] = mods_path
-
 
     def applyModOrder(self):
         i = 1
@@ -177,24 +194,28 @@ class DragApp(QWidget):
         for mod in loaded_mods:
             names_array.append(mod[0])
         for mod in self.ddm.stringList():
-            mod_index = names_array.index(mod) # index of mod in big array
+            mod_index = names_array.index(mod)  # index of mod in big array
             mod_path = loaded_mods[mod_index][1]
             if sorted_pattern.match(mod):
                 # Mod was sorted previously, replace prefix
-                mod_name = f'{f'{i}':0>3} {mod[4:]}'
+                mod_name = f"{i:03} {mod[4:]}"
             else:
-                mod_name = f'{f'{i}':0>3} {mod}'
+                mod_name = f"{i:03} {mod}"
             mod_xml = ET.parse(f"{mods_path}/{mod_path}/metadata.xml")
             root = mod_xml.getroot()
             root.find("name").text = mod_name
-            mod_xml.write(f"{mods_path}/{mod_path}/metadata.xml", encoding='utf-8', xml_declaration=True)
+            mod_xml.write(
+                f"{mods_path}/{mod_path}/metadata.xml",
+                encoding="utf-8",
+                xml_declaration=True,
+            )
             i += 1
         self.getModList()
 
-
     def getModList(self):
         # Get list of Isaac mods
-        if mods_path == '': return
+        if mods_path == "":
+            return
         mod_list = os.listdir(mods_path)
         if loaded_mods != []:
             loaded_mods.clear()
@@ -203,10 +224,9 @@ class DragApp(QWidget):
             root = mod_xml.getroot()
             if [root.find("name").text, mod] not in loaded_mods:
                 loaded_mods.append([root.find("name").text, mod])
-        
+
         loaded_mods.sort()
         self.ddm.setStringList([mod[0] for mod in loaded_mods])
-
 
     def get_accent_color_hex(self):
         app = QApplication.instance()
@@ -218,17 +238,17 @@ class DragApp(QWidget):
 
 
 def set_icon(app):
-    if sys.platform == 'win32':
-        app.setWindowIcon(QIcon('assets/icon.ico'))
-    elif sys.platform == 'darwin':
-        app.setWindowIcon(QIcon('assets/icon.icns'))
+    if sys.platform == "win32":
+        app.setWindowIcon(QIcon("assets/icon.ico"))
+    elif sys.platform == "darwin":
+        app.setWindowIcon(QIcon("assets/icon.icns"))
     else:
-        app.setWindowIcon(QIcon('assets/icon.png'))
+        app.setWindowIcon(QIcon("assets/icon.png"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyle('fusion')
+    app.setStyle("fusion")
     set_icon(app)
     window = DragApp()
     window.show()
